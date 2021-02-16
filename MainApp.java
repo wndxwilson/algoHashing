0d16a4d34303fed7485fc64d8d1c3a2aead51f89
unknown <taiwilson5@gmail.com>
2021-02-16 23:32:22 +0800

    added bank dataset

--- MainApp.java
+++ MainApp.java
@@ -6,39 +6,76 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.UUID;
 
 public class MainApp {
 	public static void main(String[] args) throws FileNotFoundException {
 		// params
-		int begin = 1;
-		int end = 5;
+		int begin = 50;
+		int end = 1000;
 
-		double load_factor = 1;
+		double load_factor = 0.5;
+		int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 		// Generate dataset
 		Random rand = new Random();
 		List<Integer[]> dataset = new ArrayList<Integer[]>();
 		for (int i = begin; i <= end; i++) {
-			Integer[] list = { i*5, rand.nextInt(500) };
+			Integer[] list = { i+90123400, rand.nextInt(500) };
 			dataset.add(list);
 		}
 		
-
+		List<String[]> records = new ArrayList<>();
+		String row;
+		BufferedReader csvReader = new BufferedReader(new FileReader("BankChurners.csv"));
+		try {
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				// do something with the data
+				records.add(data);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			csvReader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		records.remove(0);
+	
 		//Create Hashing(load_factor,max_list_size,hashin_type)
-		Hashing h = new Hashing(load_factor,dataset.size(),false);
+		Hashing h = new Hashing(load_factor,records.size(),false);
+
+			//add node
+			for(String[] data : records)
+			{
+				h.addNode(Integer.parseInt(data[0]),Integer.parseInt(data[2]));
+			}
+
+		//search , -1 if not found
+		int x = h.search(20);
+		if(x == -1){
+			System.out.println("Not found");
+		}
+		//Create Hashing(load_factor,max_list_size,hashin_type)
+		Hashing h2 = new Hashing(load_factor,records.size(),true);
 
 		//add node
-		for(Integer[] data : dataset)
+		for(String[] data : records)
 		{
-			h.addNode(data[0],data[1]);
+			h2.addNode(Integer.parseInt(data[0]),Integer.parseInt(data[2]));
 		}
 
 		//Print out the hash map for debug purpose
 		//h.printNode();
 
 		//search , -1 if not found
-		int x = h.search(20);
-		System.out.println(x);
+		x = h2.search(0);
+		if(x == -1){
+			System.out.println("Not found");
+		}
+	
 	}
 }
 	
